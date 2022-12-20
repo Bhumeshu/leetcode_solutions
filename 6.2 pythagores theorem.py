4deefd258082ed945627b37fe1d0cf3fac27ca40
Bhumeshu <bhumeshu.23@outlook.com>
2022-12-20 13:13:11 +0530

    hypo and hypomath

---
+++ 6.2 pythagores theorem.py
@@ -0,0 +1,13 @@
+import math
+
+
+def hypo(x,y,z):
+    z = x**2 + y**2
+    result = math.sqrt(z)
+    return result
+
+def hypomath(x,y,z)
+    z = math.pow(x,2) + math.pow(y,2)
+    result = math.sqrt(z)
+    return result
+
