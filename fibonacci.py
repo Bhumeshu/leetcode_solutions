2ad14f0d4a288a278d4cc36299183ae4482d5371
Bhumeshu <bhumeshu.23@outlook.com>
2022-12-20 18:21:58 +0530

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,13 @@
 def fibonacci(n):
+    assert n != 0, "invalid"
+    # if n == 0:
+    #     raise Exception
     if n == 1:
         return int(0)
     if n == 2:
         return int(1)
+    # input("Are you sure? please hit enter to proceed")
     return fibonacci(n-2) + fibonacci(n-1)
 
 
-print(fibonacci(19))
+print(fibonacci(5))
