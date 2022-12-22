d8a814928ef000a031bfa3e01743bb1d64276c49
Bhumeshu <bhumeshu.23@outlook.com>
2022-12-22 16:23:52 +0530

    split method

--- list_split.py
+++ list_split.py
@@ -1,6 +1,10 @@
-#ip = [1, 2, 3, 4, 5, 6, 7, 8]
-#op = [1, 2, 3, 4] [5, 6, 7, 8]
-def list_split():
+ip = "sam is dead"
+# op = [1, 2, 3, 4] [5, 6, 7, 8]
+# vip = "sam is dead"
 
 
-print(list_split())
+def list_split(arr):
+    return arr.split()
+
+
+print(list_split(ip))
