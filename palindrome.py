b80f6360011e2c6ad82b90dba39cbbb6533406dc
Bhumeshu <bhumeshu.23@outlook.com>
2022-12-22 13:30:48 +0530

    palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,20 @@
 def palindrome_check(word):
-    for i in range(int(len(word)/2)):
-        if word[i] != word[-i-1]:
-            return False
-    return True
+    count = 0
+    # for i in range(int(len(word)/2)):
+    #     count += 1
+    #     if word[i] != word[-i-1]:
+    #         return False, count
+    # return True, count
+    a = list(word)
+    b = list(word)
+    print(b)
+    b.reverse()
+    print(b)
+    if a == b:
+        return True
+    else:
+        return False
 
 
-d = "wowa"
+d = "9424927"
 print(palindrome_check(d))
